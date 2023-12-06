96351e1dafea8dad871d80fe26c54b6ace8d3b9e
Thushan Fernando <thushan@thushanfernando.com>
2023-12-06 20:29:35 +1100

    lint and no profiling.

--- internal/smash/profiler.go
+++ internal/smash/profiler.go
@@ -4,16 +4,23 @@ import (
 	"log"
 	"net/http"
 	"net/http/pprof"
+	"time"
 )
 
 func InitialiseProfiler() {
 	http.DefaultServeMux = http.NewServeMux()
 	go func() {
+		address := "localhost:1984"
+		server := &http.Server{
+			Addr:         address,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+		}
 		http.HandleFunc("/debug/pprof/", pprof.Index)
 		http.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 		http.HandleFunc("/debug/pprof/profile", pprof.Profile)
 		http.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 		http.HandleFunc("/debug/pprof/trace", pprof.Trace)
-		log.Fatal(http.ListenAndServe("localhost:1984", nil))
+		log.Fatal(server.ListenAndServe())
 	}()
 }
